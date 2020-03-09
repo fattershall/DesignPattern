71b9826dfbfe38008e81e8648642b5ebaae9a995
pengh <16031791@qq.com>
2020-03-09 22:46:56 +0800

    第二节 观察者模式

---
+++ src/com/nngas/pattern/observer/ObServer.java
@@ -0,0 +1,17 @@
+package com.nngas.pattern.observer;
+
+/**
+ * @author pengh
+ * @date 2020-03-09 -22:43
+ */
+
+/**
+ * 观察者模式：系统中存在比较多的多对一情况，被依赖的对象为subject，依赖对象是observer，subject 通知 observer变化情况
+ * 1 像定牛奶业务
+ * 2 奶站 subject ，用户 observer
+ * 3 subject 登记注册、移除和通知
+ * 4 observer 接受输入
+ * 5 改造当前程序
+ */
+public class ObServer {
+}
