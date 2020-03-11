2108e836e3faeb82f30062e2d182a74f731f2a9a
pengh <16031791@qq.com>
2020-03-11 23:04:43 +0800

    第三节 装饰者模式

---
+++ src/com/nngas/pattern/Decorator/Decorator.java
@@ -0,0 +1,15 @@
+package com.nngas.pattern.Decorator;
+
+/**
+ * @author pengh
+ * @date 2020-03-11 -21:45
+ */
+
+/**
+ * 定义：装饰者模式：动态的将功能附加到对象身上，在扩展对象的功能方面，比继承更加具有弹性
+ * 就像打包一个快递 装饰者用到了主体对象 用递归的方式来获取被装饰的对象
+ * 1 主体：陶瓷、衣服 component
+ * 2 包装：将填充物物品装箱、打包、放入泡沫 decorator
+ */
+public class Decorator {
+}
---
+++ src/com/nngas/pattern/Decorator/Drink.java
@@ -0,0 +1,10 @@
+package com.nngas.pattern.Decorator;
+
+/**
+ * @author pengh
+ * @date 2020-03-11 -22:23
+ */
+public abstract class Drink {
+    
+
+}
