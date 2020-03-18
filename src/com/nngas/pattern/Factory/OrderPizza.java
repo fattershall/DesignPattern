bfa7370f42a0228739ea00dac737f0473b942451
pengh <16031791@qq.com>
2020-03-18 23:48:03 +0800

    第五节 工厂模式

--- src/com/nngas/pattern/Factory/OrderPizza.java
+++ src/com/nngas/pattern/Factory/OrderPizza.java
@@ -2,7 +2,10 @@ package com.nngas.pattern.Factory;
 
 /**
  * @author pengh
- * @date 2020-03-18 -23:33
+ * 依赖抽象原则
+ * 1 变量不要持有具体类的引用
+ * 2 不要让类继承至具体的类，应该继承抽象类或者接口
+ * 3 不要覆盖基类中已经实现的方法
  */
 public class OrderPizza {
     AbstractFactory abstractFactory;
