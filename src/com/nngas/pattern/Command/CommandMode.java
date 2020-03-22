cbd352d27f9a6163a2fade2a32e60c7952ce04c0
pengh <16031791@qq.com>
2020-03-22 19:17:06 +0800

    第六节 命令模式

---
+++ src/com/nngas/pattern/Command/CommandMode.java
@@ -0,0 +1,9 @@
+package com.nngas.pattern.Command;
+
+/**
+ * @author pengh
+ * 命令模式：将一个请求封装为一个对象，从而让我们可用不同的请求对客户进行参数化
+ * 命令模式的意义： 将外围设备和sdk封装成接口和实现类的对象，抽象实现类，将请求和外设解耦了
+ */
+public class CommandMode {
+}
